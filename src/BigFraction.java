eb22b8cde80f99ab5406d7fd46dedc287255bf03
ECantDo <eric.opseth@gmail.com>
2024-12-30 23:17:43 -0600

    Base implementation

--- src/BigFraction.java
+++ src/BigFraction.java
@@ -1,8 +1,42 @@
+/**
+ * BigFraction uses two integers to make a fraction
+ */
 public class BigFraction {
 
     int numerator, denominator;
 
     //==================================================================================================================
+    // Constructors
+    //==================================================================================================================
 
+    public BigFraction() {
+        this(0, 1);
+    }
+
+    public BigFraction(int num, int denom) {
+        this.numerator = num;
+        this.denominator = denom;
+    }
+
+    //==================================================================================================================
+    // Modify
+    //==================================================================================================================
+
+    //==================================================================================================================
+    // Convert
+    //==================================================================================================================
+
+    @Override
+    public String toString(){
+        String out = "" + this.numerator;
+        if (this.denominator != 1) {
+            out += "/" + this.denominator;
+        }
+        return out;
+    }
+
+    public double toFloat(){
+        return this.numerator/(double)this.denominator;
+    }
 
 }
